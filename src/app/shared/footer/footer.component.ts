3d47fc4c3a78600e841f707bcd3da22d45adbd81
daviddev25 <david.werner@david-werner.dev>
2024-12-28 16:41:09 +0100

    updated and refactored some code

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,11 +1,41 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import {
+  RouterLink,
+  RouterModule,
+  ActivatedRoute,
+  Router,
+} from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, RouterLink],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
-export class FooterComponent {}
+export class FooterComponent {
+  private router = inject(Router);
+  private route = inject(ActivatedRoute);
+
+  redirectGithub() {
+    window.open('https://github.com/DavidDev25');
+  }
+
+  redirectLinkedIn() {
+    window.open('www.linkedin.com/in/david-werner-01a88032a');
+  }
+
+  mailTo() {
+    window.location.href = 'mailto:${email}?subject=${subject}&body=${body}';
+  }
+
+  scrollToTop(): void {
+    if (
+      this.router.url.includes('privacy') ||
+      this.router.url.includes('legal')
+    ) {
+      this.router.navigate(['/']);
+    }
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+}
