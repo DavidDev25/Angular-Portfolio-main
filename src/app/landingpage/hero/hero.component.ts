5d90f71c5748bfc6a914dec90605a2a310ea2e27
daviddev25 <david.werner@david-werner.dev>
2024-12-28 17:14:34 +0100

    added redirection to contactMeSection

--- src/app/landingpage/hero/hero.component.ts
+++ src/app/landingpage/hero/hero.component.ts
@@ -1,16 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, RouterModule, Router } from '@angular/router';
 
 @Component({
   selector: 'app-hero',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss',
 })
 export class HeroComponent {
+  private router = inject(Router);
+  private route = inject(ActivatedRoute);
+
   scrollToBottom(): void {
     window.scrollTo({
       top: document.body.scrollHeight,
       behavior: 'smooth',
     });
+  }
+  navigateToSection(fragment: string): void {
+    if (
+      this.router.url.includes('privacy') ||
+      this.router.url.includes('legal')
+    ) {
+      this.router.navigate(['/'], { fragment: fragment }).then(() => {
+        this.scrollToElement(fragment);
+      });
+    } else {
+      this.scrollToElement(fragment);
+    }
+  }
+
+  private scrollToElement(fragment: string): void {
+    setTimeout(() => {
+      const element = document.getElementById(fragment);
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+      }
+    }, 100);
   }
 }
