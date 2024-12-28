0d0a9efff9131a435bf1d56517321e155f7c509e
daviddev25 <david.werner@david-werner.dev>
2024-12-28 21:11:33 +0100

    added function to the lets talk button which redirect to to contactMeSection

--- src/app/landingpage/why-me/why-me.component.ts
+++ src/app/landingpage/why-me/why-me.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
-
+import { Router, RouterModule, ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-why-me',
-  imports: [TranslateModule],
+  imports: [TranslateModule, RouterModule],
   templateUrl: './why-me.component.html',
   styleUrl: './why-me.component.scss',
 })
-export class WhyMeComponent {}
+export class WhyMeComponent {
+  private router = inject(Router);
+  private route = inject(ActivatedRoute);
+
+  navigateToSection(fragment: string): void {
+    if (
+      this.router.url.includes('privacy') ||
+      this.router.url.includes('legal')
+    ) {
+      this.router.navigate(['/'], { fragment: fragment }).then(() => {
+        this.scrollToElement(fragment);
+      });
+    } else {
+      this.scrollToElement(fragment);
+    }
+  }
+
+  private scrollToElement(fragment: string): void {
+    setTimeout(() => {
+      const element = document.getElementById(fragment);
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+      }
+    }, 100);
+  }
+}
